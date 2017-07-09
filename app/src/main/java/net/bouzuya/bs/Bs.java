11750e661a595470090a4dbcb8aee95224c4625d
bouzuya <m@bouzuya.net>
2017-07-09 14:47:03 +0900

    add Bs#equals & #hashCode

--- app/src/main/java/net/bouzuya/bs/Bs.java
+++ app/src/main/java/net/bouzuya/bs/Bs.java
@@ -15,11 +15,29 @@ class Bs {
         return new Bs(createdAt, note);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Bs bs = (Bs) o;
+
+        if (!createdAt.equals(bs.createdAt)) return false;
+        return note.equals(bs.note);
+    }
+
     public Instant getCreatedAt() {
         return createdAt;
     }
 
     public String getContent() {
         return note;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = createdAt.hashCode();
+        result = 31 * result + note.hashCode();
+        return result;
     }
 }
--- app/src/test/java/net/bouzuya/bs/BsTest.java
+++ app/src/test/java/net/bouzuya/bs/BsTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.threeten.bp.Instant;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
 
 public class BsTest {
@@ -13,5 +14,16 @@ public class BsTest {
         Bs bs = Bs.of(createdAt, "note");
         assertThat(bs.getContent(), is("note"));
         assertThat(bs.getCreatedAt(), is(createdAt));
+    }
+
+    @Test
+    public void testEquals() {
+        Instant createdAt = Instant.parse("2006-01-02T22:04:05Z");
+        Bs bs1 = Bs.of(createdAt, "note");
+        Bs bs2 = Bs.of(createdAt, "note");
+        Bs bs3 = Bs.of(createdAt, "note2");
+        assertThat(bs1, is(bs1));
+        assertThat(bs1, is(bs2));
+        assertThat(bs1, is(not(bs3)));
     }
 }
