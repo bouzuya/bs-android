02c507d20949cf26959d21f349377b503a66d445
bouzuya <m@bouzuya.net>
2017-07-11 07:20:20 +0900

    fix BsApplicationComponent

--- app/src/main/java/net/bouzuya/bs/android/BsApplicationComponent.java
+++ app/src/main/java/net/bouzuya/bs/android/BsApplicationComponent.java
@@ -2,8 +2,11 @@ package net.bouzuya.bs.android;
 
 import net.bouzuya.bs.android.activity.MainActivity;
 
+import javax.inject.Singleton;
+
 import dagger.Component;
 
+@Singleton
 @Component(modules = {BsApplicationModule.class})
 public interface BsApplicationComponent {
     void inject(MainActivity activity);
